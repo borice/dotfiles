63ff81c6350fba42ff8290f69c8a7b6f9a9ac5eb
Boris Capitanu <capitanu@illinois.edu>
2022-11-27 23:46:46 -0600

    Another reorg

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # install powerline font for vim
-mkdir -p $HOME/.local/share/fonts/NerdFonts && curl -fLo "$HOME/.local/share/fonts/NerdFonts/JetBrains Mono Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf
+mkdir -p ~/.local/share/fonts/NerdFonts && curl -fLo "$HOME/.local/share/fonts/NerdFonts/JetBrains Mono Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf
 
 # install coursier
 CS_TEMP=$(mktemp)
@@ -9,43 +9,43 @@ $CS_TEMP setup -y
 rm -f $CS_TEMP
 
 if [[ "$(uname -s)" == "Linux" ]]; then
-	if [ -f "/etc/debian_version" ]; then
-		# install required packages
-		sudo apt -y install git fzf bat exa fd-find tree vim curl fontconfig
+  if [ -f "/etc/debian_version" ]; then
+    # install required packages
+    sudo apt -y install git fzf bat exa fd-find tree vim curl fontconfig
 
-		git clone https://github.com/borice/dotfiles.git ~/.dotfiles --depth=1
+    git clone https://github.com/borice/dotfiles.git ~/.dotfiles --depth=1
 
-		if type -P fzf >/dev/null; then
-			ln -s $HOME/.dotfiles/fzf.bash $HOME/.fzf.bash
-		fi
+    if type -P fzf >/dev/null; then
+      ln -s ~/.dotfiles/fzf.bash ~/.fzf.bash
+    fi
 
-		if type -P fc-cache >/dev/null; then
-			fc-cache -vf $HOME/.local/share/fonts/NerdFonts
-		fi
+    if type -P fc-cache >/dev/null; then
+      fc-cache -vf ~/.local/share/fonts/NerdFonts
+    fi
 
-		cat <<EOF >>$HOME/.bashrc
+    cat <<EOF >>~/.bashrc
 
-if [ -f \$HOME/.dotfiles/common_bashrc ]; then
-	source \$HOME/.dotfiles/common_bashrc
+if [ -f ~/.dotfiles/common_bashrc ]; then
+  source ~/.dotfiles/common_bashrc
 fi
 EOF
 
-		cat <<EOF >>$HOME/.bash_aliases
+    cat <<EOF >>~/.bash_aliases
 
-if [ -f \$HOME/.dotfiles/common_aliases ]; then
-	source \$HOME/.dotfiles/common_aliases
+if [ -f ~/.dotfiles/common_aliases ]; then
+  source ~/.dotfiles/common_aliases
 fi
 EOF
 
-	fi
+  fi
 fi
 
 if [ -n "$BASH_VERSION" ]; then
-	# download bash-git-prompt
-	git clone https://github.com/magicmonty/bash-git-prompt.git ~/.repos/bash-git-prompt --depth=1
+  # download bash-git-prompt
+  git clone https://github.com/magicmonty/bash-git-prompt.git ~/.repos/bash-git-prompt --depth=1
 
-	# download z.sh
-	git clone https://github.com/rupa/z.git ~/.repos/z --depth=1
+  # download z.sh
+  git clone https://github.com/rupa/z.git ~/.repos/z --depth=1
 fi
 
-echo "All done"
+echo "All done, log out and log back in to activate changes"
